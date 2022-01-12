e9f3ade95f677f417c23a4993c6d53a8e9ad8776
Brett Bertrand <brett.bertrand@oit.edu>
2022-01-11 17:50:26 -0800

    Lab Init

---
+++ GeoQuiz/MainActivity.cs
@@ -0,0 +1,111 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Android.Util;
+
+namespace GeoQuiz
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light", MainLauncher =
+       true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private const string TAG = "Main_Activity";
+
+        int currentIndex = 0;
+        protected override void OnStart() //called when app starts
+        {
+            base.OnStart();
+            Log.Debug(TAG, "OnStart Called");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Log.Debug(TAG, "OnResume Called");
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            Log.Debug(TAG, "OnPause Called");
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            Log.Debug(TAG, "OnStop Called");
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            Log.Debug(TAG, "OnDestroy Called");
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            Log.Debug(TAG, "OnSaveInstanceState");
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Questions questionBank = new Questions();
+
+            Button trueButton = FindViewById<Button>(Resource.Id.true_button);
+            Button falseButton = FindViewById<Button>(Resource.Id.false_button);
+            Button nextButton = FindViewById<Button>(Resource.Id.next_button);
+            TextView questionTextView = FindViewById<TextView>(Resource.Id.question_text_view);
+
+            questionTextView.SetText(questionBank.questions[currentIndex], null);
+
+            trueButton.Click += delegate
+            {
+                if(questionBank.answers[currentIndex] == true)
+                {
+                    Toast.MakeText(this, Resource.String.correct_toast,
+                   ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, Resource.String.incorrect_toast,
+                    ToastLength.Short).Show();
+                }
+                
+            };
+            falseButton.Click += delegate
+            {
+                if (questionBank.answers[currentIndex] == false)
+                {
+                    Toast.MakeText(this, Resource.String.correct_toast,
+                   ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, Resource.String.incorrect_toast,
+                    ToastLength.Short).Show();
+                }
+            };
+            nextButton.Click += delegate
+            {
+                currentIndex = (currentIndex + 1) % questionBank.questions.Count;
+                questionTextView.SetText(questionBank.questions[currentIndex], null);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[]
+            permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode,
+               permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        } 
+    }
+}
---
+++ GeoQuiz/Questions.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoQuiz
+{
+    class Questions
+    {
+        public Questions()
+        {
+            Context context = Android.App.Application.Context;
+            questions.Add(context.Resources.GetText(Resource.String.question_australia));
+            answers.Add(true);
+            questions.Add(context.Resources.GetText(Resource.String.question_oceans));
+            answers.Add(true);
+
+            questions.Add(context.Resources.GetText(Resource.String.question_mideast));
+            answers.Add(false);
+            questions.Add(context.Resources.GetText(Resource.String.question_africa));
+            answers.Add(false);
+
+            questions.Add(context.Resources.GetText(Resource.String.question_americas));
+            answers.Add(true);
+            questions.Add(context.Resources.GetText(Resource.String.question_asia));
+            answers.Add(true);
+        }
+
+
+        public List<string> questions = new List<string>();
+        public List<bool> answers = new List<bool>();
+    }
+}
